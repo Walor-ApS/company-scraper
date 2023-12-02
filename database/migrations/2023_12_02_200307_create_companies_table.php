6b30ba921ff1086d160017bbe07d14c8d48cd804
nowalor <no@walor.io>
2023-12-02 21:11:35 +0100

    add company migration & model

---
+++ database/migrations/2023_12_02_200307_create_companies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('municipality_id')->constrained()->nullOnDelete();
+
+            $table->string('country')->default('DK');
+            $table->string('cvr');
+            $table->timestamp('founded_at')->nullable();
+            $table->string('ended_at')->nullable();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('city')->nullable();
+            $table->string('company_type')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->boolean('advertising_protected')->default(false);
+
+            $table->string('address');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
