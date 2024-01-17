9a88b1654e03a368af629dd914b915059dbf6964
Alexander Rubino <alexrubino05@gmail.com>
2024-01-17 19:54:19 +0100

    Updatted small changes

--- app/Http/Controllers/CompaniesController.php
+++ app/Http/Controllers/CompaniesController.php
@@ -14,9 +14,9 @@ class CompaniesController extends Controller
 {
     public function fetchCompanies(): JsonResponse {   
       //Danish
-      //37-49
-      $url = 'https://www.proff.dk/segmentering?numEmployeesFrom=51&numEmployeesTo=52&mainUnit=true';
-      (new CompanyService())->setup('https://www.proff.dk', $url, 'DK');
+      // //37-49
+      // $url = 'https://www.proff.dk/segmentering?numEmployeesFrom=51&numEmployeesTo=52&mainUnit=true';
+      // (new CompanyService())->setup('https://www.proff.dk', $url, 'DK');
 
       // //220-249
       // $url = 'https://www.proff.dk/segmentering?numEmployeesFrom=220&numEmployeesTo=249&mainUnit=true';
@@ -31,19 +31,19 @@ class CompaniesController extends Controller
       // $url = 'https://www.proff.se/segmentering?numEmployeesFrom=220&numEmployeesTo=249';
       // (new CompanyService())->setup('https://www.proff.se', $url, 'SV');
       
-      //Norway
-      //37-49
-      $url = 'https://www.proff.no/segmentering?numEmployeesFrom=51&numEmployeesTo=52&mainUnit=true';
-      (new CompanyService())->setup('https://www.proff.no', $url, 'NO');
+      // //Norway
+      // //37-49
+      // $url = 'https://www.proff.no/segmentering?numEmployeesFrom=51&numEmployeesTo=52&mainUnit=true';
+      // (new CompanyService())->setup('https://www.proff.no', $url, 'NO');
       
       // //220-249
       // $url = 'https://www.proff.no/segmentering?numEmployeesFrom=220&numEmployeesTo=249&mainUnit=true';
       // (new CompanyService())->setup('https://www.proff.no', $url, 'NO');
       
-      //Finland
-      //20-49
-      $url = 'https://www.proff.fi/segmentointi?employeeRange=50%20-%2099';
-      (new CompanyService())->setup('https://www.proff.fi', $url, 'FI');
+      // //Finland
+      // //20-49
+      // $url = 'https://www.proff.fi/segmentointi?employeeRange=50%20-%2099';
+      // (new CompanyService())->setup('https://www.proff.fi', $url, 'FI');
       
       // //159-249
       // $url = 'https://www.proff.fi/segmentointi?employeeRange=100%20-%20249';
