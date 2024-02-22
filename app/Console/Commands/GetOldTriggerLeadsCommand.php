0e0a2ac00a4b90ece998feef39ddf5768f66355d
nowalor <no@walor.io>
2024-02-22 14:02:09 +0100

    modify command

--- app/Console/Commands/GetOldTriggerLeadsCommand.php
+++ app/Console/Commands/GetOldTriggerLeadsCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Actions\EmployeeCheckAction;
 use App\Models\Company;
 use App\Models\TriggerLead;
 use Illuminate\Console\Command;
@@ -16,46 +17,51 @@ class GetOldTriggerLeadsCommand extends Command
         $companies = $this->getCompanies();
 
        foreach($companies as $company) {
-           $newCompanyEmployees = $company
-               ->employeeHistory()
-               ->where("employees", ">", "49")
-               ->first()
-               ->employees;
-
-           TriggerLead::create([
-               'company_id' => $company->id,
-               'employees' => $newCompanyEmployees,
-               'country' => $company->country,
-               'year' => $company->created_at->format('Y'),
-               'month' => $company->created_at->format('F')
-           ]);
+           (new EmployeeCheckAction())->excecute($company);
+//           $newCompanyEmployees = $company
+//               ->employeeHistory()
+//               ->where("employees", ">", "49")
+//               ->first()
+//               ->employees;
+//
+//           $newCompanyEmployeesCount = $newCompanyEmployees !== null ? $this->employeeRoundDown($company->employees) : $this->employeeRangeRoundDown($company->employees_range);
+//
+//
+//           TriggerLead::create([
+//               'company_id' => $company->id,
+//               'employees' => $newCompanyEmployeesCount,
+//               'country' => $company->country,
+//               'year' => $company->created_at->format('Y'),
+//               'month' => $company->created_at->format('F')
+//           ]);
        }
     }
 
     private function getCompanies(): Collection
     {
-        $companies = Company::select(["id"])
-            ->with("employeeHistory")
-            ->get();
-
-        $matchingCompanies = [];
-
-        foreach ($companies as $company) {
-            $hasBelow = $company
-                ->employeeHistory()
-                ->where("employees", "<", "50")
-                ->exists();
-
-            $hasAbove = $company
-                ->employeeHistory()
-                ->where("employees", ">", "49")
-                ->exists();
-
-            if ($hasBelow && $hasAbove) {
-                $matchingCompanies[] = $company->id;
-            }
-        }
-
-        return Company::whereIn('id', $matchingCompanies)->get();
+        return Company::all();
+//        $companies = Company::select(["id"])
+//            ->with("employeeHistory")
+//            ->get();
+//
+//        $matchingCompanies = [];
+//
+//        foreach ($companies as $company) {
+//            $hasBelow = $company
+//                ->employeeHistory()
+//                ->where("employees", "<", "50")
+//                ->exists();
+//
+//            $hasAbove = $company
+//                ->employeeHistory()
+//                ->where("employees", ">", "49")
+//                ->exists();
+//
+//            if ($hasBelow && $hasAbove) {
+//                $matchingCompanies[] = $company->id;
+//            }
+//        }
+//
+//        return Company::whereIn('id', $matchingCompanies)->get();
     }
 }
