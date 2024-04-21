72a2b44f214cf892c67722ee53a31543884da21a
nowalor <no@walor.io>
2024-04-21 17:44:15 +0200

    modify command

--- app/Models/CompanyEmployee.php
+++ app/Models/CompanyEmployee.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CompanyEmployee extends Model
 {
     use HasFactory;
     protected $fillable = ['year', 'month', 'employees', 'company_id', 'week', 'employees_range'];
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
