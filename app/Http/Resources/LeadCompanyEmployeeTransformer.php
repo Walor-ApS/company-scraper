a30c214130c01e14b0b650f60fed4593d8e2b383
nowalor <no@walor.io>
2024-04-22 10:37:38 +0200

    latest

---
+++ app/Http/Resources/LeadCompanyEmployeeTransformer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Actions\TranslateIconNames;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LeadCompanyEmployeeTransformer extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $webInfo = (new TranslateIconNames())->index($this->company->country);
+
+        $cvr = preg_replace("/[^0-9]/", "", $this->company->cvr);
+        $website = "{$webInfo['company_url']}/$cvr";
+
+        return [
+            'cvr' => $this->company->cvr,
+            'name' => $this->company->name,
+            'employees' => $this->employees,
+            'website' => $website,
+        ];
+    }
+}
