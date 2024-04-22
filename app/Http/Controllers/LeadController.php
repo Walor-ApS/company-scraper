efdc958b941d937a29244450991855d202ef5701
nowalor <no@walor.io>
2024-04-22 12:10:50 +0200

    test

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -20,7 +20,6 @@ class LeadController extends Controller
         $employees = CompanyEmployee::whereIn('company_id', $employeesTest)
             ->take(50)
             ->where('employees', '>', 50)
-//            ->where('employees', '<', 85)
             ->whereHas('company', function($q) use($country){
                 $q->where('country', $country);
             })
