3485af2eb8c71507e8320662e8930946d5a3e896
nowalor <no@walor.io>
2024-04-22 10:59:01 +0200

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('fetch-employees')->weekly();
+        $schedule->command('fetch-employees')->dailyAt('11:00');
     }
 
     /**
